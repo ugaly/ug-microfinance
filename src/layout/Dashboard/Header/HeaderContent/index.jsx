27c862377c15974a40d485d8d727013d76c36879
zacharia <zmwalwama@gmail.com>
2025-02-21 16:13:44 +0300

    asd

--- src/layout/Dashboard/Header/HeaderContent/index.jsx
+++ src/layout/Dashboard/Header/HeaderContent/index.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Box from '@mui/material/Box';
@@ -17,6 +17,7 @@ import MegaMenuSection from './MegaMenuSection';
 import useConfig from 'hooks/useConfig';
 import { MenuOrientation } from 'config';
 import DrawerHeader from 'layout/Dashboard/Drawer/DrawerHeader';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
 
 // ==============================|| HEADER - CONTENT ||============================== //
 
@@ -28,13 +29,42 @@ export default function HeaderContent() {
   const localization = useMemo(() => <Localization />, []);
 
   const megaMenu = useMemo(() => <MegaMenuSection />, []);
+  const { defaultOfficeId, setDefaultOfficeId } = useState('');
+
 
   return (
     <>
+    
       {menuOrientation === MenuOrientation.HORIZONTAL && !downLG && <DrawerHeader open={true} />}
+      
       {!downLG && <Search />}
       {/* {!downLG && megaMenu} */}
       {/* {!downLG && localization} */}
+
+      <div>
+
+        
+
+      <FormControl size="small" sx={{ minWidth: 300 }}>
+        <InputLabel>Chagua Tawi</InputLabel>
+          <Select
+                value={defaultOfficeId || ''}
+                onChange={(e) => {
+                  sessionStorage.setItem('defaultOfficeId', e.target.value);
+                  setDefaultOfficeId(e.target.value);
+                }}
+                label="Chagua Tawi"
+              >
+            <MenuItem value="">All</MenuItem>
+            <MenuItem value="1">Product 1</MenuItem>
+            <MenuItem value="2">Product 2</MenuItem>
+          </Select>
+        </FormControl>
+
+        
+
+
+      </div>
       {downLG && <Box sx={{ width: '100%', ml: 1 }} />}
 
       {/* <Notification /> */}
@@ -43,6 +73,7 @@ export default function HeaderContent() {
       {/* <Customization /> */}
       {!downLG && <Profile />}
       {downLG && <MobileSection />}
+      
     </>
   );
 }
